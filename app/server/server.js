a54cca3eb0f7e7e6892011b112aff945e9e9fb8d
hyang <hymeldon@gmail.com>
2018-04-03 13:47:12 -0400

    Add database connection in server.js.

--- app/server/server.js
+++ app/server/server.js
@@ -3,11 +3,44 @@
 const fs = require('fs'),
       helmet = require('helmet');
 
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 3000,
+      env = app.get('env');
+
+const mongoose = require('mongoose');
+
 const config = require('./config');
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const database = ((config) => {
+    let c = config.database[env];
+    let db = c.db || 'test',
+        port = c.port || '27017',
+        host = c.host.join(`:${port},`) || 'localhost',
+        user = c.user || '',
+        pwd = c.pwd || '',
+        extra = c.extra || '';
+
+    return {
+        "db": db, "port": port, "host": host,
+        "user": user, "pwd": pwd, "extra": extra
+    };
+})(config);
+
+const dbConnection = ((db) => {
+    let userString = '';
+    if (db.user != '' && db.pwd != '')
+        userString = `${db.user}:${db.pwd}@`;
+    
+    let connection = `mongodb://${userString}${db.host}:${db.port}/${db.db}`;
+    if (db.extra != '')
+        connection += `?${db.extra}`;
+
+    //console.log(connection);
+    return mongoose.connect(connection);
+})(database);
+
+console.log("--------------");
 
 app.use(helmet());
 
@@ -21,11 +54,18 @@ if (config.server.staticFile) {
     let path = config.server.staticPath;
     if (path === undefined || !fs.existsSync(path)) path = __dirname + "/../../public";
     app.use(express.static(path));
-    app.use('*', (req, res) => { res.sendFile('index.html', { root: path }) });
+    app.use('*', (req, res) => { res.sendFile('index.html', { root: path }); });
 }
 
-app.listen(port, () => {
-    console.log('Server is running. Listening port ' + port);
-});
+dbConnection.then((res) => {
+    console.log('Database connected.');
 
+    app.locals.db = mongoose;
+    app.listen(port, () => {
+        console.log('Server is running. Listening port ' + port);
+    });
+
+}, (err) => {
+    console.log('Cannot connect to the database.');
+})
 
